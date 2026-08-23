65e71f4a06c0c1ce5acad4628731e3a8831f7450
Liberu Software <office@liberu.co.uk>
2026-08-23 02:07:00 +0000

    Fix framework compatibility

--- src/Pages/Overview.php
+++ src/Pages/Overview.php
@@ -8,7 +8,6 @@ use Filament\Pages\Page;
 
 final class Overview extends Page
 {
-    protected static string $view = 'localization-core-filament::overview';
+    protected string $view = 'localization-core-filament::overview';
     protected static ?string $title = 'Localization';
 }
-
